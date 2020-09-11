04b8a85d966b21473ff8af81faa2e57df3aaa101
Stefan Mesken <kappadistributive@gmail.com>
2020-09-11 19:55:01 +0200

    create hdf5 store

--- model/main.py
+++ model/main.py
@@ -1,9 +1,18 @@
-from config import config
-from preprocessor import IGPreprocessor
+import argparse
 import logging
 
+from config import config
+from preprocessor import IGPreprocessor
+
 if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--force_update", type=bool, default=False)
+    args = parser.parse_args()
     logging.config.dictConfig(config["logger"])
-    preprocessor = IGPreprocessor(config)
+    print("bla:", args.force_update)
+    preprocessor = IGPreprocessor(
+        config,
+        force_update=args.force_update
+    )
 
     preprocessor.run()
--- model/preprocessor.py
+++ model/preprocessor.py
@@ -1,12 +1,15 @@
-from abc import ABC, abstractmethod
-from pathlib import Path
-from typing import Any, Dict
-from tqdm import tqdm
 import json
 import logging
 import logging.config
-import requests
 import tarfile
+from abc import ABC, abstractmethod
+from pathlib import Path
+from typing import Any, Dict
+
+import h5py
+import numpy
+import requests
+from tqdm import tqdm
 
 
 class PipelineStep(ABC):
@@ -25,7 +28,7 @@ class IGPreprocessor(PipelineStep):
     ) -> None:
         self.logger = logging.getLogger(self.__class__.__name__)
         self.config = config[self.__class__.__name__]
-        self.force_update = False
+        self.force_update = force_update
 
         self.logger.info(
             f"Initializing {self.__class__.__name__} with config\n" +
@@ -39,6 +42,7 @@ class IGPreprocessor(PipelineStep):
         )
 
         self.hdf = IGHDF(
+            raw_data_directory=self.config["raw_data_directory"],
             image_directory=(
                 Path(
                     self.config["raw_data_path"]
@@ -52,12 +56,15 @@ class IGPreprocessor(PipelineStep):
                 ).parent /
                 self.config["raw_data_directory"] /
                 "json"
-            )
+            ),
+            raw_data_group_names=self.config["raw_data_group_names"],
+            force_update=self.force_update
         )
 
     def run(self) -> None:
         self.logger.info(f"Starting {self.__class__.__name__}")
         self.loader.run()
+        self.hdf.run()
 
 
 class IGLoader(PipelineStep):
@@ -142,13 +149,17 @@ class IGHDF(PipelineStep):
 
     def __init__(
         self,
+        raw_data_directory: str,
         image_directory: Path,
         json_directory: Path,
+        raw_data_group_names: Dict[str, str],
         force_update: bool = False
     ):
         self.logger = logging.getLogger(self.__class__.__name__)
+        self.raw_data_directory = raw_data_directory
         self.image_directory = image_directory
         self.json_directory = json_directory
+        self.raw_data_group_names = raw_data_group_names
         self.force_update = force_update
 
         self.logger.info(
@@ -159,4 +170,45 @@ class IGHDF(PipelineStep):
         )
 
     def run(self) -> None:
-        pass
+        hdf5_path = (
+            Path(self.image_directory.parent) /
+            (self.raw_data_directory + ".hdf5")
+        )
+
+        if self.force_update:
+            hdf5_path.unlink(missing_ok=True)
+
+        if hdf5_path.is_file():
+            self.logger.info(
+                "Cached version of hdf5 store already exists. Skipping step."
+            )
+
+        else:
+            self.logger.info(f"Creating hdf5 store at {hdf5_path}")
+
+            with h5py.File(hdf5_path, "w") as hdf5:
+                for key, value in self.raw_data_group_names.items():
+                    hdf5_group = hdf5.create_group(value)
+                    caption_id = []
+                    caption_raw = []
+                    with open(self.json_directory / key) as f:
+                        raw_json = json.load(f)
+
+                    for user, entry in raw_json.items():
+                        for post_id, post in entry.items():
+                            caption_id.append(user + "_@_" + post_id)
+                            caption_raw.append(post["caption"])
+                    hdf5_group.create_dataset(
+                        "caption_id",
+                        data=numpy.array(
+                            caption_id,
+                            dtype=h5py.string_dtype(encoding='utf-8')
+                        )
+                    )
+                    hdf5_group.create_dataset(
+                        "caption_raw",
+                        data=numpy.array(
+                            caption_raw,
+                            dtype=h5py.string_dtype(encoding='utf-8')
+                        )
+                    )
